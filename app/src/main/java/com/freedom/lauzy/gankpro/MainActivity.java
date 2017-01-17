a3d173c7da8a4f15f33cb04078526a5631644724
Lauzy <freedompaladin@gmail.com>
2017-01-17 22:33:07 +0800

    initial commit

---
+++ app/src/main/java/com/freedom/lauzy/gankpro/MainActivity.java
@@ -0,0 +1,13 @@
+package com.freedom.lauzy.gankpro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/freedom/lauzy/gankpro/base/BaseActivity.java
@@ -0,0 +1,54 @@
+package com.freedom.lauzy.gankpro.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by Lauzy on 2017/1/17.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(setLayoutId());
+        ButterKnife.bind(this);
+        initViews();
+        loadData();
+    }
+
+    protected abstract int setLayoutId();
+
+    protected abstract void loadData();
+
+    protected abstract void initViews();
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
