9c2b7542077213b8c2cebc37e28798ef6d05d982
Lauzy <freedompaladin@gmail.com>
2017-03-01 09:07:06 +0800

    add

---
+++ app/src/main/java/com/freedom/lauzy/gankpro/function/CollapsingToolbarState.java
@@ -0,0 +1,11 @@
+package com.freedom.lauzy.gankpro.function;
+
+/**
+ * Created by Lauzy on 2017/2/27.
+ */
+
+public enum CollapsingToolbarState {
+    EXPANDED,
+    COLLAPSED,
+    INTERMEDIATE
+}
