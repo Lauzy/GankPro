584f7bfa68f1fe6c407e16a142bd49732fbf6e14
Lauzy <freedompaladin@gmail.com>
2017-04-07 01:28:35 +0800

    theme and collection bug

--- app/src/main/java/com/freedom/lauzy/gankpro/function/view/AndroidItemDecoration.java
+++ app/src/main/java/com/freedom/lauzy/gankpro/function/view/AndroidItemDecoration.java
@@ -2,7 +2,6 @@ package com.freedom.lauzy.gankpro.function.view;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.support.v4.content.ContextCompat;
