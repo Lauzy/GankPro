eec2df6553ca7fce1ac6f7694a318b66017185ae
Lauzy <freedompaladin@gmail.com>
2017-05-05 11:47:14 +0800

    debug

--- app/src/main/java/com/freedom/lauzy/gankpro/ui/MainActivity.java
+++ app/src/main/java/com/freedom/lauzy/gankpro/ui/MainActivity.java
@@ -1,12 +1,9 @@
 package com.freedom.lauzy.gankpro.ui;
 
-import android.animation.Animator;
-import android.app.Activity;
 import android.content.res.Configuration;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
@@ -17,23 +14,32 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.freedom.lauzy.gankpro.R;
 import com.freedom.lauzy.gankpro.common.base.BaseToolbarActivity;
+import com.freedom.lauzy.gankpro.common.utils.NetUtils;
 import com.freedom.lauzy.gankpro.common.widget.behavior.GankBehavior;
-import com.freedom.lauzy.gankpro.common.widget.behavior.GankBottomBehavior;
 import com.freedom.lauzy.gankpro.function.MineTitleListener;
 import com.freedom.lauzy.gankpro.function.utils.SharePrefUtils;
 import com.freedom.lauzy.gankpro.ui.fragment.AndroidFragment;
 import com.freedom.lauzy.gankpro.ui.fragment.BeautyFragment;
 import com.freedom.lauzy.gankpro.ui.fragment.CategoryFragment;
 import com.freedom.lauzy.gankpro.ui.fragment.MineFragment;
-import com.tencent.tinker.lib.tinker.TinkerInstaller;
+
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import butterknife.OnClick;
+import cn.jpush.android.api.JPushInterface;
+import cn.jpush.android.api.TagAliasCallback;
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Action1;
+import rx.functions.Func1;
 
 public class MainActivity extends BaseToolbarActivity implements BottomNavigationView
         .OnNavigationItemSelectedListener, MineTitleListener {
@@ -54,8 +60,11 @@ public class MainActivity extends BaseToolbarActivity implements BottomNavigatio
     private AndroidFragment mAndroidFragment;
     private MineFragment mMineFragment;
     private GankBehavior mGankBehavior;
-    private GankBottomBehavior mGankBottomBehavior;
+    //    private GankBottomBehavior mGankBottomBehavior;
     private String mTitle;
+
+    private static final int MSG_SET_ALIAS = 0x0011;
+    private static final int MSG_SET_TAGS = 0x0012;
 
     @Override
     protected int getLayoutResId() {
@@ -66,12 +75,175 @@ public class MainActivity extends BaseToolbarActivity implements BottomNavigatio
     protected void loadData() {
         /*TinkerInstaller.onReceiveUpgradePatch(getApplicationContext(), Environment.getExternalStorageDirectory().getAbsolutePath()
         +"/gank_patch.apk");*/
+        //推送设置别名，实际项目中可根据登录信息，可在登录成功后将token等信息作为标签或别名，此处模拟，设置固定值
+
+        String tag = "TAG1,TAG2,TAG3";
+        String[] sArray = tag.split(",");
+        Set<String> tagSet = new LinkedHashSet<>();
+        Collections.addAll(tagSet, sArray);
+
+        /*mHandler.sendMessage(mHandler.obtainMessage(MSG_SET_ALIAS, "Gank"));
+        mHandler.sendMessage(mHandler.obtainMessage(MSG_SET_TAGS, tagSet));*/
+
+
+        Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(final Subscriber<? super String> subscriber) {
+                Log.e(LYTAG, "call: ");
+                JPushInterface.setAliasAndTags(getApplicationContext(), "Gank2", null, new GankTagAliasCallback(new TagCallback() {
+                    @Override
+                    public void onSuccess() {
+                        subscriber.onNext("Gank2");
+                        subscriber.onCompleted();
+                        Log.e(LYTAG, "onSuccess: ");
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        subscriber.onError(throwable);
+                        Log.e(LYTAG, "onError: ");
+                    }
+                }));
+            }
+        }).retry(2).subscribe();
+
     }
+
+
+    public class RetryWithDelay implements
+            Func1<Observable<? extends Throwable>, Observable<?>> {
+
+        private final int maxRetries;
+        private final int retryDelayMillis;
+        private int retryCount;
+
+        public RetryWithDelay(int maxRetries, int retryDelayMillis) {
+            this.maxRetries = maxRetries;
+            this.retryDelayMillis = retryDelayMillis;
+        }
+
+        @Override
+        public Observable<?> call(Observable<? extends Throwable> attempts) {
+            return attempts.flatMap(new Func1<Throwable, Observable<?>>() {
+                @Override
+                public Observable<?> call(Throwable throwable) {
+                    if (++retryCount <= maxRetries) {
+                        return Observable.timer(retryDelayMillis, TimeUnit.MILLISECONDS);
+                    }
+                    return Observable.error(throwable);
+                }
+            });
+        }
+    }
+
+    interface TagCallback {
+        void onSuccess();
+
+        void onError(Throwable throwable);
+    }
+
+    class GankTagAliasCallback implements TagAliasCallback {
+
+        private TagCallback mTagCallback;
+
+        public GankTagAliasCallback(TagCallback tagCallback) {
+            mTagCallback = tagCallback;
+        }
+
+        @Override
+        public void gotResult(int i, String s, Set<String> set) {
+            switch (i) {
+                case 0:
+                    mTagCallback.onSuccess();
+                    break;
+                case 6002:
+                    mTagCallback.onError(new Throwable("FAIL"));
+                    break;
+            }
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private Handler mHandler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_SET_ALIAS:
+                    Log.d(LYTAG, "Set alias in handler.");
+                    JPushInterface.setAliasAndTags(getApplicationContext(), (String) msg.obj, null, mAliasCallback);
+                    break;
+                case MSG_SET_TAGS:
+                    Log.d(LYTAG, "Set tags in handler.");
+                    JPushInterface.setAliasAndTags(getApplicationContext(), null, (Set<String>) msg.obj, mTagsCallback);
+                    break;
+                default:
+                    Log.i(LYTAG, "Unhandled msg - " + msg.what);
+            }
+            return false;
+        }
+    });
+
+    private final TagAliasCallback mAliasCallback = new TagAliasCallback() {
+
+        @Override
+        public void gotResult(int code, String alias, Set<String> tags) {
+            String logs;
+            switch (code) {
+                case 0:
+                    logs = "Set tag success";
+                    Log.i(LYTAG, logs);
+                    break;
+
+                case 6002:
+                    logs = "Failed to set alias due to timeout. Try again after 60s.";
+                    Log.i(LYTAG, logs);
+                    if (NetUtils.isNetworkConnected(getApplicationContext())) {
+                        mHandler.sendMessageDelayed(mHandler.obtainMessage(MSG_SET_ALIAS, alias), 1000 * 60);
+                    } else {
+                        Log.i(LYTAG, "No network");
+                    }
+                    break;
+
+                default:
+                    logs = "Failed with errorCode = " + code;
+                    Log.e(LYTAG, logs);
+            }
+        }
+    };
+
+    private final TagAliasCallback mTagsCallback = new TagAliasCallback() {
+
+        @Override
+        public void gotResult(int code, String alias, Set<String> tags) {
+            String logs;
+            switch (code) {
+                case 0:
+                    logs = "Set alias success";
+                    Log.i(LYTAG, logs);
+                    break;
+
+                case 6002:
+                    logs = "Failed to set tags due to timeout. Try again after 60s.";
+                    Log.i(LYTAG, logs);
+                    if (NetUtils.isNetworkConnected(getApplicationContext())) {
+                        mHandler.sendMessageDelayed(mHandler.obtainMessage(MSG_SET_TAGS, tags), 1000 * 60);
+                    } else {
+                        Log.i(LYTAG, "No network");
+                    }
+                    break;
+
+                default:
+                    logs = "Failed with errorCode = " + code;
+                    Log.e(LYTAG, logs);
+            }
+        }
+
+    };
 
     /**
      * 解决重启Activity时fragment重叠的问题
      *
-     * @param savedInstanceState
+     * @param savedInstanceState savedInstanceState
      */
     @Override
     protected void loadFragment(Bundle savedInstanceState) {
@@ -155,7 +327,7 @@ public class MainActivity extends BaseToolbarActivity implements BottomNavigatio
     @Override
     protected void initViews() {
         mGankBehavior = GankBehavior.from(mToolbar);
-        mGankBottomBehavior = GankBottomBehavior.from(mBottomMainNavigation);
+//        mGankBottomBehavior = GankBottomBehavior.from(mBottomMainNavigation);
         MenuItem item = mBottomMainNavigation.getMenu().getItem(0);
         onNavigationItemSelected(item);//默认选中第一个
         mBottomMainNavigation.setOnNavigationItemSelectedListener(this);
